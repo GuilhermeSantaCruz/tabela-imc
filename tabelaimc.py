c281991ca6a036aef3beb30c69d64ac9fa60ac8f
GuilhermeSantaCruz <guilhermelucaslsc@gmail.com>
2025-11-02 16:29:57 -0300

    Update tabelaimc.py

--- tabelaimc.py
+++ tabelaimc.py
@@ -4,9 +4,13 @@ altura = float(input("Qual a sua altura? [m] "))
 imc = peso / (altura ** 2)
 print(f"Seu IMC é {imc:.2f}")
 if imc < 18.5:
-    print("Abaixo do peso")
-elif imc >= 18.5 and imc < 24.9:
-    print("Peso normal")
-elif imc > 25 and imc < 29.9:
-    print("Sobrepeso")
+    print("Abaixo do peso ")
+elif imc < 25:
+    print("Peso ideal ")
+elif imc < 30:
+    print("Sobrepeso ")
+elif imc < 40:
+    print("Obesidade ")
+else:
+    print("Obesidade mórbida ")
             
