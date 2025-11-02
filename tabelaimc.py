f54c1df6ffd1d367939fbf39b219e263e661d9c9
GuilhermeSantaCruz <guilhermelucaslsc@gmail.com>
2025-11-02 16:28:16 -0300

    Update tabelaimc.py

--- tabelaimc.py
+++ tabelaimc.py
@@ -1,7 +1,12 @@
-print("*-" * 35)
-print("VAMOS CALCULAR SEU IMC")
-print("*-" * 35)
 nome = input("Qual o seu nome? ")
 peso = float(input(f"Qual o seu peso {nome}? [Kg] "))
 altura = float(input("Qual a sua altura? [m] "))
 imc = peso / (altura ** 2)
+print(f"Seu IMC é {imc:.2f}")
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc >= 18.5 and imc < 24.9:
+    print("Peso normal")
+elif imc > 25 and imc < 29.9:
+    print("Sobrepeso")
+            
